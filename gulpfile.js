517c35314f3d8af8af2ce0d1b4a3242ea84ea542
Manny Hsu <a83a83cjcj88256@gmail.com>
2020-01-04 14:18:37 +0800

    menu overlay

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,10 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
-const wait = require('gulp-wait');
-//added 'wait'
 
 function css() {
     return gulp
         .src('scss/app.scss')
-        .pipe(wait(1500))
-        //added 'wait'
         .pipe(autoprefixer() )
         .pipe(sass({ outputStyle: "expanded" }))
         .pipe(gulp.dest("dist/css"))
